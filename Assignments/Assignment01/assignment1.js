be9ea7a4cc490268179bae2f806e02891e4b3e85
Logan Kim <logan.kim0212@gmail.com>
2018-02-06 03:32:52 -0500

    Assignment01

--- Assignments/Assignment01/assignment1.js
+++ Assignments/Assignment01/assignment1.js
@@ -1,4 +1,33 @@
 //Javascript Source Code
+
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 function CalculateMetric() {
 
     var gender, weight, height, age, level, firstop, secondop, thirdop, total, calories;
@@ -7,42 +36,47 @@ function CalculateMetric() {
     weight = document.getElementById("weightMetric").value;
     height = document.getElementById("heightMetric").value;
     age = document.getElementById("ageMetric").value;
-    level = document.getElementById("levelMetric").value;
+    level = document.getElementsByClassName("levelMetric").value;
 
     if (gender == "male")
     {
         firstop = 13.75 * weight;
         secondop = 5.003 * height;
         thirdop = 6.755 * age;
-        total = 66 + firstop + secondop - thirdop;
+        total = 66.47 + firstop + secondop - thirdop;
         calories = total * level;
     }
     else if (gender == "female") {
         firstop = 9.563 * weight;
-        secondop = 1.850 * height;
+        secondop = 1.85 * height;
         thirdop = 4.676 * age;
         total = 655.1 + firstop + secondop - thirdop;
         calories = total * level;
     }
-    document.getElementById("BMR").innerHTML = calroies.toFixed(2);
+    alert('Your body will burn ' + total.toFixed(2) + ' calories each day. Based upon this, your daily calorie requirement would be ' + calories.toFixed(2) + ' calories.');
 }
 
 function CalculateImperial() {
 
-    var gender, weight, height, age, level, firstop, secondop, thirdop, total, calories;
+    var gender, age, level, firstop, secondop, thirdop, total, calories, stone, pound, inch, feet, height, weight, feetToInch, stoneToPound;
 
     gender = document.getElementById("genderImperial").value;
-    weight = document.getElementById("weightImperial").value;
-    height = document.getElementById("heightImperial").value;
+    stone = document.getElementById("stoneImperial").value;
+    pound = document.getElementById("poundImperial").value;
+    feet = document.getElementById("feetImperial").value;
+    inch = document.getElementById("inchImperial").value;
     age = document.getElementById("ageImperial").value;
-    level = document.getElementById("levelImperial").value;
+    level = document.getElementsByClassName("levelImperial").value;
+
+    height = parseInt(feet) * 12 + parseInt(inch);
+    weight = parseInt(stone) * 14 + parseInt(pound);
 
     if (gender == "male")
     {
-        firstop = 6.2 * weight;
+        firstop = 6.24 * weight;
         secondop = 12.7 * height;
-        thirdop = 6.76 * age;
-        total = 66.5 + firstop + secondop - thirdop;
+        thirdop = 6.755 * age;
+        total = 66.47 + firstop + secondop - thirdop;
         calories = total * level;
     }
     else if (gender == "female") {
@@ -52,5 +86,6 @@ function CalculateImperial() {
         total = 655.1 + firstop + secondop - thirdop;
         calories = total * level;
     }
-    document.getElementById("BMR1").innerHTML = calroies.toFixed(2);
+
+    alert('Your body will burn ' + total.toFixed(2) + ' calories each day. Based upon this, your daily calorie requirement would be ' + calories.toFixed(2) + ' calories.');
 }
