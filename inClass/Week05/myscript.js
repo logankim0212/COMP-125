a90dbaf607a490bc529d81497b18978fa01b705e
Logan Kim <logan.kim0212@gmail.com>
2018-02-21 12:40:06 -0500

    edit

---
+++ inClass/Week05/myscript.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+
+var list = document.getElementById("navList");
+var contact = document.createElement("li");
+contact.innerHTML = "Contact";
+
+var new1 = document.querySelecterAll()
+list.appandChild(contact);
